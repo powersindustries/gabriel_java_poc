944514b0649e56f0e6e8434259dc451ee6df1e63
powersindustries <lp.accounts@fastmail.com>
2024-12-12 09:05:09 -0600

    Added functionality for sending HTML emails.

--- src/main/java/com/powersindustries/gabriel/controller/EmailSendingController.java
+++ src/main/java/com/powersindustries/gabriel/controller/EmailSendingController.java
@@ -3,6 +3,7 @@ package com.powersindustries.gabriel.controller;
 import com.powersindustries.gabriel.model.User;
 import com.powersindustries.gabriel.service.EmailSendingService;
 import com.powersindustries.gabriel.service.UserService;
+import jakarta.mail.MessagingException;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -13,14 +14,21 @@ public class EmailSendingController {
     private final EmailSendingService emailSendingService;
     private final UserService userService;
 
-    public EmailSendingController(EmailSendingService emailSendingService, UserService userService) {
+    public EmailSendingController(EmailSendingService emailSendingService, UserService userService) throws MessagingException {
         this.emailSendingService = emailSendingService;
         this.userService = userService;
 
         // ToDo: This block is for POC. Move to endpoint.
         List<User> userList = userService.getAllUsers();
         for (User user : userList) {
-            emailSendingService.sendEmail(user.getEmail(), "Test", "Test message.");
+            emailSendingService.sendtTextEmail(user.getEmail(), "Test", "Test message.");
+
+            String html = "<div>" +
+                    "<h1>Hello world, from Gabriel<h1>" +
+                    "<p>Here's some test text.<p>" +
+                    "<div>";
+            emailSendingService.sendtHTMLEmail(user.getEmail(), "Test html", html);
+
         }
     }
 }
--- src/main/java/com/powersindustries/gabriel/service/EmailSendingService.java
+++ src/main/java/com/powersindustries/gabriel/service/EmailSendingService.java
@@ -1,8 +1,11 @@
 package com.powersindustries.gabriel.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +14,23 @@ public class EmailSendingService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String body) {
+    public void sendtTextEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
+
+        mailSender.send(message);
+    }
+
+    public void sendtHTMLEmail(String to, String subject, String html) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(html, true);
 
         mailSender.send(message);
     }
